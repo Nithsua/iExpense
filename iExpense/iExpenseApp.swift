fdc9bbedf7d1f5215c49ea4be92d257000b879d1
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-07-04 12:57:31 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nivas Muthu M G on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
